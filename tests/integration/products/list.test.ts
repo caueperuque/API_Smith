db0166b558612900b5b895dd9458a3d6ec85cb9b
caue peruque <caue19052003@gmail.com>
2023-09-13 17:52:20 -0300

    feat: req02 completed

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,17 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  it ('Deve retornar 200, caso a requisição seja feita com sucesso', async function () {
+    const response = await chai.request(app)
+      .get('/products')
+      .send();
 
+    expect(response.status).to.be.equal(200)
+  })
 });
